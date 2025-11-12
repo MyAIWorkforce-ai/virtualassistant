a4fd209e027e8c6a80c29b0242054a4b7dd3cfe1
Zoey-uiux <zainababdullah.uiux@gmail.com>
2025-11-13 01:01:58 +0500

    IU

--- src/app/components/IndustriesComponents/HeroSection.tsx
+++ src/app/components/IndustriesComponents/HeroSection.tsx
@@ -110,8 +110,9 @@ export default function HeroSection({
             className="object-cover object-center"
             priority
         />
-    </motion.motion.div>
+    </motion.div>
 </div>
+
 
   </div>
 </motion.section>
