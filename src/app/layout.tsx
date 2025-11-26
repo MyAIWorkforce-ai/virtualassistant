93fef5baca3cdecf6b9f30e117fe9b78906a436b
Zainab Abdullah <zainababdullah.uiux@gmail.com>
2025-11-26 08:35:44 +0500

    Microsoft Clarity Snippet added to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WDM9BTM7');</script>
  {/* End Google Tag Manager */}
+
+          {/* Microsoft Clarity (head) */}
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ublhev30pz");
+</script>
+        {/* End Microsoft Clarity */}
       </head>
       
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
