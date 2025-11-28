64b41eba80c406fe7ea357d55d2d026e6d76fbc9
Zainab Abdullah <zainababdullah.uiux@gmail.com>
2025-11-28 21:14:34 +0500

    Scroll Reset layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/_components/Navbar";
 import Footer from "@/app/_components/Footer";
+import ScrollReset from "./utils/scroll-reset";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -63,6 +64,7 @@ export default function RootLayout({
           }}
         />
         <Navbar />
+        <ScrollReset />
         {children}
         <Footer />
       </body>
