b701f77956ab33d7305fdeb3087bd7139171b83a
Zainab Abdullah <zainababdullah.uiux@gmail.com>
2025-11-28 21:53:09 +0500

    Updated imports in industriesClient.tsx

--- src/app/industries/industriesClient.tsx
+++ src/app/industries/industriesClient.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
 import { motion } from "framer-motion";
+import { useEffect } from "react";
 
 export default function MainIndustry() {
 
