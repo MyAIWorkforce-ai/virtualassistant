3ccb6db15d44173f58658659579c632af1c3e83a
Zainab Abdullah <zainababdullah.uiux@gmail.com>
2025-11-29 09:29:05 +0500

    GLOBAL SCHEMA: Organization + Website updated in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,54 @@ export default function RootLayout({
             __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "ublhev30pz");`,
           }}
         />
+
+                {/* GLOBAL SCHEMA: Organization + Website */}
+        <Script
+          id="global-organization-schema"
+          type="application/ld+json"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "VirtualAssistant.com.au",
+              url: "https://virtualassistant.com.au/",
+              logo: "https://virtualassistant.com.au/logo.png",
+              sameAs: [
+                "https://www.facebook.com/",
+                "https://www.instagram.com/",
+                "https://www.linkedin.com/",
+              ],
+              contactPoint: {
+                "@type": "ContactPoint",
+                contactType: "customer support",
+                email: "support@virtualassistant.com.au",
+                availableLanguage: ["English"],
+              },
+            }),
+          }}
+        />
+
+        <Script
+          id="global-website-schema"
+          type="application/ld+json"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "VirtualAssistant.com.au",
+              url: "https://virtualassistant.com.au/",
+              potentialAction: {
+                "@type": "SearchAction",
+                target:
+                  "https://virtualassistant.com.au/?s={search_term_string}",
+                "query-input": "required name=search_term_string",
+              },
+            }),
+          }}
+        />
+        
       </head>
 
       <body
