cdfbb2589b1a4539ffee99a8b4b8a8caf2d2e09f
Zainab Abdullah <zainababdullah.uiux@gmail.com>
2025-11-30 13:21:38 +0500

    Update industriesClient.tsx

--- src/app/industries/industriesClient.tsx
+++ src/app/industries/industriesClient.tsx
@@ -146,7 +146,7 @@ const TradesServicescards = [
 const HospitalityOther = [
   {
     icon: "/industrymain-images/resturant.png",
-    title: "Restaurant",
+    title: "Restaurant & Takeaway Businesses",
     desc: "Reservation Management Made Easy.  Manages table bookings, cancellations, and automated waitlists.",
     link: "/industries/resturant",
   },
