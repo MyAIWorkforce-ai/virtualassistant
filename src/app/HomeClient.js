4bb20f61675285dd8d0cc5d2c970c5c51d7a31f0
Zainab Abdullah <zainababdullah.uiux@gmail.com>
2025-11-30 14:11:28 +0500

    Update HomeClient.js

--- src/app/HomeClient.js
+++ src/app/HomeClient.js
@@ -566,8 +566,8 @@ useEffect(() => {
       {expanded && (
         <div className="mt-1 text-[#797A7D] text-sm">
           - Analytics & insights, calls, bookings, <br/> &nbsp; cancellations, no-shows, performance<br/>
-          - One clean dashboard replaces multiple tools <span className="sm:ml-0 md:ml-2 lg:ml-2"> and </span> platforms.<br/>
-          - Front-End Prompts, Easily update what your &nbsp;&nbsp; AI says to clients on the phone, website, or <br/> &nbsp; chat — no tech skills required.<br/>
+          - One clean dashboard replaces multiple tools <span className="sm:ml-0 md:ml-2 lg:ml-2"> and </span> <span className="sm:ml-1 md:ml-2 lg:ml-2"> platforms.</span><br/>
+          - Front-End Prompts, Easily update what your &nbsp;&nbsp; AI says to <span className="sm:ml-2 md:ml-0 lg:ml-0"> clients </span> on the phone, website, or <br/> &nbsp; chat — no tech skills required.<br/>
           - Fully Customisable Branding, Upload your <br/> &nbsp; business logo, set your theme colours, and <br/> &nbsp; brand the dashboard to feel like your own <br/> &nbsp; system — not just another generic tool.
         </div>
       )}
