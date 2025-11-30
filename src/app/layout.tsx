d73e05fcf8cfc706a0a7281116c0305aca1ceec2
Zainab Abdullah <zainababdullah.uiux@gmail.com>
2025-11-30 17:29:13 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/og-image.png", 
-        width: 1200,
-        height: 630,
+        url: "/image/logo1.png", 
+      width: {1200},
+        height: {630},
         alt: "AI Virtual Receptionist Preview",
       },
     ],},
