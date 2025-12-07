dc507070623a201cc426e7ed6cf7676cb0349bd1
Zainab Abdullah <zainababdullah.uiux@gmail.com>
2025-12-07 22:53:09 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({
 <Script
   id="organization-schema-client"
   type="application/ld+json"
-  strategy="afterInteractive"
+  strategy="beforeInteractive"
   dangerouslySetInnerHTML={{
     __html: JSON.stringify({
       "@context": "https://schema.org",
@@ -105,7 +105,7 @@ export default function RootLayout({
 <Script
   id="website-schema-client"
   type="application/ld+json"
-  strategy="afterInteractive"
+  strategy="beforeInteractive"
   dangerouslySetInnerHTML={{
     __html: JSON.stringify({
       "@context": "https://schema.org",
