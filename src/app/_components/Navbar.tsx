91a39d71c6ea6797105f8cf6ed47a0f1b8a05d5a
Zainab Abdullah <zainababdullah.uiux@gmail.com>
2025-12-24 02:18:36 +0500

    Fix casing for 'Concreters' and 'HVAC' in Navbar

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -135,7 +135,7 @@ export default function Navbar() {
                 {[
                   ["physiotherapy", "myotherapist", "personaltrainer", "massagetherapist", "psychology", "chiropractor", "podiatrist", "beautytherapist", "hairdresser", "dentist"],
                   ["osteopaths", "lawyer", "accountant", "consultant", "realestateagent", "mechanic", "plumber", "cleaning", "electricians", "resturant"],
-                  [ "hotelsaccommodation", "barber", "builders", "concreters", "hvac", "landscapers", "roofing", "forother"]
+                  [ "hotelsaccommodation", "barber", "builders", "Concreters", "hvac", "landscapers", "roofing", "forother"]
                 ].map((column, idx) => (
                   <ul key={idx}>
                     {column.map((slug) => (
@@ -300,7 +300,7 @@ export default function Navbar() {
                     "physiotherapy", "myotherapist", "personaltrainer", "massagetherapist", "psychology", "chiropractor",
                     "podiatrist", "beautytherapist", "hairdresser", "dentist", "osteopaths", "lawyer", "accountant",
                     "consultant", "realestateagent", "mechanic", "plumber", "cleaning", "electricians", "resturant",
-                    "hotelsaccommodation", "Barber", "Builders", "Concreters", "HVAC", "Landscapers", "Roofing", "forother",
+                    "hotelsaccommodation", barber", "builders", "Concreters", "hvac", "landscapers", "roofing", "forother",
                   ].map((slug) => (
                     <li key={slug}>
                       <Link
